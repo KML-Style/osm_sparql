2380755f9be29bfb1650979f624a8147cbcc9645
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-07-24 10:44:32 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,4 +1,4 @@
-// Request 1: Get all amenities of a specific type and their distance to the user’s coordinates.
+// Request 1.1: Get all amenities of a specific type and their distance to the user’s coordinates.
 export async function getNearbyAmenities(endpointUrl, latUser, lonUser, amenity) {
   const sparqlQuery = `
     PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
@@ -25,7 +25,7 @@ export async function getNearbyAmenities(endpointUrl, latUser, lonUser, amenity)
   return data;
 }
 
-// Request 2: Get all amenities of a specific type located less than 'distMax' meters from a public transport stop.
+// Request 1.2: Get all amenities of a specific type located less than 'distMax' meters from a public transport stop.
 export async function getAmenitiesNextToTransport(endpointUrl, amenity, distMax) {
     const sparqlQuery = `
     PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
@@ -71,7 +71,7 @@ export async function getAmenitiesNextToTransport(endpointUrl, amenity, distMax)
     return data;
 }
 
-// Request 3: Get all security camera located less than 'distMax' meters from a given object.
+// Request 1.3: Get all security camera located less than 'distMax' meters from a given object.
 export async function getNearbySurveillance(endpointUrl, id, distMax){
   const sparqlQuery = `
     PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
@@ -115,7 +115,7 @@ export async function getNearbySurveillance(endpointUrl, id, distMax){
     return data;
   }
 
-// Request 4: Get opening times for a given list of objects (caracterized by id)
+// Request 1.5: Get opening times for a given list of objects (characterized by an ID)
 export async function getOpeningTime(endpointUrl, buildings){
     const valuesBuildings = buildings.join('\n        ');
     const sparqlQuery = `
@@ -145,7 +145,7 @@ export async function getOpeningTime(endpointUrl, buildings){
   return data;
   }
 
-// Request 5: Get number of levels for a given list of objects (caracterized by id)
+// Request 1.5-bis: Get number of levels for a given list of objects (characterized by an ID)
 export async function getNumberOfLevels(endpointUrl, buildings){
     const valuesBuildings = buildings.join('\n        ');
     const sparqlQuery = `
@@ -170,6 +170,70 @@ export async function getNumberOfLevels(endpointUrl, buildings){
   
     if (!response.ok) throw new Error(`SPARQL Error: ${response.status}`);
     const data = await response.json();
+  
+  return data;
+  }
+
+// Request 1.6: Get the elements (nodes and segments) that are inside an area delimitated by a list of buildings (characterized by an ID)
+export async function getItemsInArea(endpointUrl, buildings){
+    const valuesBuildings = buildings.join('\n        ');
+    const sparqlQuery = `
+    PREFIX osmkey: <https://www.openstreetmap.org/wiki/Key:>
+    PREFIX osmway: <https://www.openstreetmap.org/way/>
+    PREFIX osmnode: <https://www.openstreetmap.org/node/>
+    PREFIX osmrel: <https://www.openstreetmap.org/relation/>
+    PREFIX ogc: <http://www.opengis.net/rdf#>
+
+    SELECT ?element ?name WHERE{
+      VALUES ?area {
+        ${valuesBuildings}
+      }
+      ?area ogc:sfCovers ?element .
+      OPTIONAL { ?element osmkey:name ?name. } 
+    }
+    ORDER BY (!BOUND(?name)) ?name
+    `;
+
+    const fullUrl = endpointUrl + "?query=" + encodeURIComponent(sparqlQuery);
+    const headers = { "Accept": "application/sparql-results+json" };
+
+    const response = await fetch(fullUrl, { headers });
+  
+    if (!response.ok) throw new Error(`SPARQL Error: ${response.status}`);
+    const data = await response.json();
     
   return data;
   }
+
+// Request 1.7: Get the elements (nodes and segments) that are inside an area delimitated by a polygon.
+export async function getItemsInPolygon(endpointUrl, polygon){
+    const formattedPolygon = formatWKT(polygon,"POLYGON");
+    const sparqlQuery = `
+    PREFIX osmkey: <https://www.openstreetmap.org/wiki/Key:>
+    PREFIX osmway: <https://www.openstreetmap.org/way/>
+    PREFIX osmnode: <https://www.openstreetmap.org/node/>
+    PREFIX osmrel: <https://www.openstreetmap.org/relation/>
+    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+
+    SELECT ?element ?name WHERE {
+      BIND(${formattedPolygon} AS ?area)
+      ?element geo:hasGeometry/geo:asWKT ?geom .
+      OPTIONAL { ?element osmkey:name ?name. }
+    
+      FILTER(geof:sfWithin(?geom,?area ))
+      
+    }
+    ORDER BY (!BOUND(?name)) ?name
+    `;
+    console.log(sparqlQuery);
+    const fullUrl = endpointUrl + "?query=" + encodeURIComponent(sparqlQuery);
+    const headers = { "Accept": "application/sparql-results+json" };
+
+    const response = await fetch(fullUrl, { headers });
+  
+    if (!response.ok) throw new Error(`SPARQL Error: ${response.status}`);
+    const data = await response.json();
+    
+    return data;
+}
