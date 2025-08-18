ed006a0c62a1cdadfc3fc347fa3cbf68b182f2b5
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-08-18 11:27:02 +0200

    Create meteoHistorical.js

---
+++ meteoHistorical.js
@@ -0,0 +1,17 @@
+export async function getMeteoHistorical(lat, lon, start_date, end_date) {
+    // Date format : YYYY-MM-DD
+    const url = `
+    https://archive-api.open-meteo.com/v1/archive?latitude=${lat}&longitude=${lon}&start_date=${start_date}&end_date=${end_date}&daily=sunrise,sunset,wind_speed_10m_max,precipitation_sum,temperature_2m_mean,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&timezone=auto
+    `
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error : cannot fetch meteo data.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error :", error);
+    return null;
+  }
+}
